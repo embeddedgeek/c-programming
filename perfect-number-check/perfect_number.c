5dd10b891d5728da493060f837ca64e9797cb65b
Shan <kingsantanu [at] gmail dot com>
2016-11-23 20:42:44 -0800

    minor updates to perferct-num-check
    Updated some typos and descriptions  in  perfect_number.c

--- perfect-number-check/perfect_number.c
+++ perfect-number-check/perfect_number.c
@@ -24,9 +24,10 @@ extern int perfect_num_check(int num){
   }
 
   // If num = sum of its proper positive divisors,then,the number num is a perfect number
-  // E.g., 6 is a perfect number, because 1, 2, and 3 are  and 1 + 2 + 3 = 6
-  // If sum = num, function perfect_num() will return sum for a perfect number
-  // If sum is not equal to num, the given number num is not a perfect number and the function will return 1
+  // E.g., 6 is a perfect number, because 1, 2, and 3  and 1 + 2 + 3 = 6
+  // If sum = num, function perfect_num_check() will return 1 for a perfect number
+  // If sum is not equal to num, the given number num is not a perfect number and the function will return 0
+
 
   if(sum==num){
 	   printf("Computed result = %d is a perfect number \n",num);
