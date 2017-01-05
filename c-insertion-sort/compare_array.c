7e638a7f0a4a6c482aad8d571d99a348e5252a7b
EmbeddedGeeks <embeddedsysgeeks@gmail.com>
2017-01-05 14:19:51 -0800

    Add files via uploaded
    Insertion sort files added

---
+++ c-insertion-sort/compare_array.c
@@ -0,0 +1,39 @@
+/* ================================================================================================================
+ * Name: compare_array.c
+ * Description: A simple function to compare two arrays of size n
+ * Input(s):
+ *   *arr1 : integer pointer to array
+ *   *arr2 : integer pointer to array
+ *   n : integer number (size of array)
+ * Output(s):
+ *   Returns true(flag=1)/false(flag=0)
+ * License: MIT
+ * Reference: http://embeddedgeeks.com/c-programming/c-programming-example-insertion-sort
+ *
+ * ================================================================================================================
+ */
+
+
+#include<stdio.h>
+#include"insertion_sort.h"
+
+// A simple function to compare two arrays of same size n
+int compare_array(int *arr1, int *arr2, int n)
+{
+   int i,flag = 0;
+   for (i=0;i<n;i++){
+
+  	 	if(arr1[i] == arr2[i])
+  	 	flag=1;
+  	 }
+   return flag;
+}
+
+
+
+
+
+
+
+
+
---
+++ c-insertion-sort/insertion_sort_test.c
@@ -0,0 +1,67 @@
+/* ================================================================================================================
+ * Name: insertion_sort_test.c
+ * Description: A simple test function for verification and validation of Insertion sort
+ * Input(s):
+ *   arr : integer array
+ * Output(s):
+ *   Display array after sorting
+ *   Check if test is passed or failed
+ * License: MIT
+ * Reference: http://embeddedgeeks.com/c-programming/c-programming-example-insertion-sort
+ *
+ * ================================================================================================================
+ */
+
+#include<stdio.h>
+#include"insertion_sort.h"
+
+
+void insertion_sort_test(void) {
+
+	int flag;
+	printf("Test function for insertion sort\n");
+
+	// Input: arr = (5, 3, 6, 9, 4, 2)
+	// Output: arr = (2, 3, 4, 5, 6, 9)
+
+	  int arr[6] = {5,3,6,9,4,2};
+	             /* To determine the number of elements in the array,
+	             we can divide the total size of the array
+	             by the size of the array element.*/
+	  int n= sizeof(arr)/sizeof(arr[0]);
+	  printf("Given array: \n");
+	  print_array(&arr[0],n);
+
+
+	// function call for test insertion sort
+    /* Passing array '&arr[0]'(by reference) and size of array 'n' (by value)
+     * function insertion_sort() arguments.*/
+
+	  insertion_sort(&arr[0],n);
+
+
+	  // Verifying and validating result
+	  // Expected sorted array (2, 3, 4, 5, 6, 9)
+	  int sorted_arr[6] = {2, 3, 4, 5, 6, 9};
+	  printf("\nAfter sorting computed result: \n");
+
+	  /* Passing array '&arr[0]' (by reference) and size of array 'n' (by value)
+	   function print_array() arguments.*/
+	  print_array(&arr[0],n);
+	  printf("\nExpected result: \n");
+	  print_array(&sorted_arr[0],n);
+
+     // Compare computed and expected result
+	  flag = compare_array(&arr[0], &sorted_arr[0], n);
+
+	 if(flag == 1){
+	 	printf("\nTest Passed\n");
+	 	}
+	 else{
+	 	printf("\nTest Failed\n");
+	 	}
+
+}
+
+
+
